65452dae4eacd3bf93cff347e00047113d91b37f
Díaz Bryan <marcos.mdahn@gmail.com>
2021-09-12 21:42:31 -0500

    Solución al problema Permutations

--- CSES/Intro/Permutations.cpp
+++ CSES/Intro/Permutations.cpp
@@ -1,8 +1,23 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+int main(){
+	int n;
 
-int main() {
-	
+	cin >> n;
+
+	if (n > 1 && n < 4){
+		cout << "NO SOLUTION";
+
+	}
+	else{
+		for(int i = 2; i <= n; i+=2){
+			cout << i << " ";
+		}
+		for(int i = 1; i <= n; i+=2){
+			cout << i << " ";
+		}
+	}
+
 	return 0;
 }
