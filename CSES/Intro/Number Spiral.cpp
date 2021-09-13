ca83008c98bb39c752c291189adbdc5699cb1bf3
Díaz Bryan <marcos.mdahn@gmail.com>
2021-09-12 21:44:18 -0500

    Solución al problema Number Spiral

---
+++ CSES/Intro/Number Spiral.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main(){
+	long long int t,mayor,columnas,renglones,solucion=0,diagonal;
+	cin >> t;
+	for(int i=0;i<t;i++){
+		cin >> renglones >> columnas;
+		mayor = max(columnas,renglones);
+		if(mayor%2==0){
+			if(mayor == columnas){
+				diagonal = mayor*mayor;
+				diagonal = diagonal - (mayor-1);
+				solucion = diagonal - (mayor-renglones);
+				//---
+			}else{
+				diagonal = mayor*mayor;
+				diagonal = diagonal - (mayor-1);
+				solucion = diagonal + (mayor-columnas);
+				//++++
+			}
+				
+		}else{
+			if(mayor == columnas){
+				diagonal = mayor*mayor;
+				diagonal = diagonal - (mayor-1);
+				solucion = diagonal + (mayor-renglones);
+				//++++
+			}else{
+				diagonal = mayor*mayor;
+				diagonal = diagonal - (mayor-1);
+				solucion = diagonal - (mayor-columnas);
+				//----
+			}
+			diagonal=0;
+		}
+		cout << solucion <<"\n";
+	}
+	return 0;
+}
