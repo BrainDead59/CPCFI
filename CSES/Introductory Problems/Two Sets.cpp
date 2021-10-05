0c05d38abba49c49d3f201cb313c4caf8094c3e5
Díaz Bryan <marcos.mdahn@gmail.com>
2021-10-05 13:27:16 -0500

    Solución al problema Two Sets

---
+++ CSES/Introductory Problems/Two Sets.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long int n;
+    scanf("%lld",&n);
+
+    if(((n*(n+1))/2)%2==1){
+        printf("NO");
+    }else{
+        printf("YES\n");
+        vector<long long int> A,B;
+        long long int limite = (n*(n+1))/4;
+
+         while (n){
+            if (limite - n>= 0){
+                A.push_back(n);
+                limite -= n;
+            }
+            else{
+                B.push_back(n);
+            }
+            n--;
+        }
+
+        printf("%d\n",A.size());
+        for(int i=0;i<A.size();i++){
+            printf("%lld ",A[i]);
+        }
+
+        printf("\n%d\n",B.size());
+        for(int i=0;i<B.size();i++){
+            printf("%lld ",B[i]);
+        }
+    }
+    return 0;
+}
