a624a14d0168278233bb1ee6b00396450477f333
Díaz Bryan <marcos.mdahn@gmail.com>
2021-10-05 13:26:46 -0500

    Solución al problema Two Knights

---
+++ CSES/Introductory Problems/Two Knights.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    for(long long int i=1;i<=n;i++){
+        printf("%lld\n",i * i * (i * i - 1) / 2 - 4 * (i - 1) * (i - 2));
+    }
+    return 0;
+}
