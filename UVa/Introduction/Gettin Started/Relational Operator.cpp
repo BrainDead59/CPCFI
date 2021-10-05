fa67c6de14000d90e53b83b3ebf93a174b32aea5
Díaz Bryan <marcos.mdahn@gmail.com>
2021-10-05 11:09:13 -0500

    Solución al problema Relational Operator

---
+++ UVa/Introduction/Gettin Started/Relational Operator.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    int n,a,b;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        scanf("%d %d",&a,&b);
+        if(a>b){
+            printf(">\n");
+        }else if(a<b){
+            printf("<\n");
+        }else if(a==b){
+            printf("=\n");
+        }
+    }
+    return 0;
+}
