087bb8e6e378aeb83ddd5344363374e0b7c918dc
Díaz Bryan <marcos.mdahn@gmail.com>
2022-02-06 22:12:40 -0600

    Solución al problema carrots

---
+++ Kattis/Difficulty/[1-2]/carrots.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,p;
+    string s;
+    scanf("%d %d",&n, &p);
+    getline(cin,s);
+    while(n--){
+        getline(cin,s);
+    }
+    printf("%d",p);
+    return 0;
+}
