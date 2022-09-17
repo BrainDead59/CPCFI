5b5aa3eef2c1953456281376becc90dfd779700d
Díaz Bryan <marcos.mdahn@gmail.com>
2022-09-16 23:19:04 -0500

    Solución del problema EchoEchoEcho

---
+++ Kattis/Difficulty/[1-2]/EchoEchoEcho.cpp
@@ -0,0 +1,23 @@
+//Kattis: EchoEcho
+
+#include <bitset>  
+#include <iostream> 
+#include <string>  
+#include <vector>  
+#include <unordered_map>
+#include <map>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+
+int main(){
+    // freopen("input.txt","r",stdin);
+    // freopen("output.txt","w",stdout);
+    string cadena;
+    cin >> cadena;
+    cout << cadena << " " << cadena << " " << cadena;
+	return 0;
+}
