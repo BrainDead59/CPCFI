92fdebe7aac13e83bacf8a1341fd8f679b041f96
Díaz Bryan <marcos.mdahn@gmail.com>
2022-09-16 23:18:17 -0500

    Solución del problema Betting

---
+++ Kattis/Difficulty/[1-2]/Betting.cpp
@@ -0,0 +1,24 @@
+//Kattis: Betting
+
+#include <bitset>  
+#include <iostream> 
+#include <string>  
+#include <vector>  
+#include <unordered_map>
+#include <map>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+
+int main(){
+    // freopen("input.txt","r",stdin);
+    // freopen("output.txt","w",stdout);
+    
+    string a;
+    cin >> a;
+    cout << a[1] << a[0];
+	return 0;
+}
