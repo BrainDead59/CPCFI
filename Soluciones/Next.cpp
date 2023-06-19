e6412a72a1bc93a8bebcb219b3a7627a1b8f3a85
Brain Dead <marcos.mdahn@gmail.com>
2023-06-19 11:03:23 -0600

    Plantillas

---
+++ Soluciones/Next.cpp
@@ -0,0 +1,46 @@
+//Codeforces: A. Insert Digit
+//g++ -std=c++17 -o a b.cpp
+//Default c++14
+
+
+#include <bitset>  
+#include <iostream> 
+#include <string>  
+#include <vector>  
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm> 
+#include <sstream> 
+#include <numeric> 
+#include <list>
+#include <stack>
+#include <deque>
+#include <queue>
+#include <iomanip> 
+#include <math.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned int uint;
+typedef unsigned long long ull;
+
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef pair<int,int> pii;
+typedef map<int,int> mii;
+
+int main(){
+    ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+
+    
+    
+    return 0;
+}
