fd625a1926333fdae194c4c3722cf644ad709af2
BrainDead <marcos.mdahn@gmail.com>
2023-08-31 12:00:00 -0600

    Solución al problema Collecting Numbers

---
+++ CSES/Sorting and Searching/Collecting Numbers.cpp
@@ -0,0 +1,56 @@
+//CSES: Collecting Numbers
+//g++ -std=c++17 -o a b.cpp
+//Default c++17
+
+#include <bitset>  
+#include <iostream> 
+#include <string>  
+#include <vector>  
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm> 
+#include <sstream> 
+#include <numeric> 
+#include <list>
+#include <stack>
+#include <deque>
+#include <queue>
+#include <iomanip> 
+#include <math.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned int uint;
+typedef unsigned long long ull;
+
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef pair<int,int> pii;
+typedef map<int,int> mii;
+
+int main(){
+    ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+    // freopen("input.txt","r",stdin);
+    // freopen("output.txt","w",stdout);
+
+    int n,contador=0;
+    cin >> n;
+    unordered_set<int> valores;
+    for(int i=0;i<n;i++){
+        int valor;
+        cin >> valor;
+        if(valores.find(valor)==valores.end()){
+            contador++;
+        }
+        valores.insert(valor+1);
+    }
+    cout << contador;
+    
+    return 0;
+}
