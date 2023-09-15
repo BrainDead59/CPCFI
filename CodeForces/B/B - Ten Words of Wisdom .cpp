97c759a2e3c6efeff7a0e944cd7b08380d8cfea9
BrainDead <marcos.mdahn@gmail.com>
2023-09-15 12:00:00 -0600

    Solución al problema Ten Words of Wisdom

---
+++ CodeForces/B/B - Ten Words of Wisdom .cpp
@@ -0,0 +1,58 @@
+//
+//g++ -std=c++17 -o a b.cpp
+//Default c++17
+
+#include <bitset>  
+#include <iostream> 
+#include <string>  
+#include <vector>  
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm> 
+#include <sstream> 
+#include <numeric> 
+#include <list>
+#include <stack>
+#include <deque>
+#include <queue>
+#include <iomanip> 
+#include <math.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned int uint;
+typedef unsigned long long ull;
+
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef pair<int,int> pii;
+typedef map<int,int> mii;
+
+int main(){
+    ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+
+    int t;
+    cin >> t;
+    for(int i=0;i<t;i++){
+        int n;
+        cin >> n;
+        priority_queue<pair<int,int>> pq;
+        for(int j=0;j<n;j++){
+            int a,b;
+            cin >> a >> b;
+            if(a<=10){
+                pq.push({b,j+1});
+            }
+        }
+        cout << pq.top().second << "\n";
+    }
+    return 0;
+}
